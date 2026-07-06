2c82c7e39126507ba43b4e7ee5bfcc625d0c0b90
tharupama <tharupama826@gmail.com>
2026-07-06 19:44:41 +0530

    establish rabbit mq to send refresh request to all micro services at ones using docker

--- emplayee-service/src/main/java/com/springTharupama/emplayee_service/controller/MessageController.java
+++ emplayee-service/src/main/java/com/springTharupama/emplayee_service/controller/MessageController.java
@@ -1,9 +1,8 @@
-package com.springTharupama.department_service.Controller;
+package com.springTharupama.emplayee_service.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +11,7 @@ public class MessageController {
     @Value("${spring.boot.message}")//in application properties(now on git)
     private String message;
 
-    @GetMapping("/message")
+    @GetMapping("/users/message")
     public String message() {
         return message;
     }
