2f02998eafd1c4f83dadd0f2bc7b63c634cce26e
tharupama <tharupama826@gmail.com>
2026-07-09 08:10:52 +0530

    implemented circuit breaker

--- emplayee-service/src/main/java/com/springTharupama/emplayee_service/servide/impl/EmployeeServiceImpl.java
+++ emplayee-service/src/main/java/com/springTharupama/emplayee_service/servide/impl/EmployeeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.springTharupama.emplayee_service.servide.impl;
 
+
 import com.springTharupama.emplayee_service.dto.ApiResponceDto;
 import com.springTharupama.emplayee_service.dto.DepartmentDto;
 import com.springTharupama.emplayee_service.dto.EmployeeDto;
@@ -7,8 +8,12 @@ import com.springTharupama.emplayee_service.entity.Employee;
 import com.springTharupama.emplayee_service.repository.EmployeeRepository;
 import com.springTharupama.emplayee_service.servide.ApiClient;
 import com.springTharupama.emplayee_service.servide.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
 @AllArgsConstructor
 
 public class EmployeeServiceImpl implements EmployeeService {
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     private ModelMapper modelMapper;//because of all args constructor constructor injection happen
 
@@ -39,9 +45,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         EmployeeDto employeeDtoSaved = modelMapper.map(savedEmployee, EmployeeDto.class);
         return employeeDtoSaved;
     }
-
+    //@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")//to retry before circuit send responsee as properties
     @Override
     public ApiResponceDto getEmployeeById(Long id) {
+        logger.info("getEmployeeById method inside");
         Employee employee = employeeRepository.findById(id).get();
 //communication with rest template
 //        ResponseEntity<DepartmentDto> responseEntity =  restTemplate.getForEntity("http://localhost:8080/api/v1/department/"
@@ -62,5 +70,15 @@ public class EmployeeServiceImpl implements EmployeeService {
         return apiResponceDto;
     }
 
+    public ApiResponceDto getDefaultDepartment(Long id,Exception exception){
+        logger.info("getDefaultDepartment method inside");
+        Employee employee = employeeRepository.findById(id).get();
+
+        DepartmentDto departmentDto = new DepartmentDto(Long.parseLong("404"),"Default Department","development department","D001");
+        EmployeeDto foundEmployeeDto = modelMapper.map(employee, EmployeeDto.class);
+
+        ApiResponceDto apiResponceDto = new ApiResponceDto(foundEmployeeDto,departmentDto);
+        return apiResponceDto;
+    }
 
 }
